0e56471a8244a3f0895e48f8b5172c3a1b6de2a8
supriya0415 <supriya.y.gzp@gmail.com>
2025-07-07 13:59:23 -0400

    ConvoApp

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+     <div className="items-center justify-center">
+      <hr className=" border-black"/>
+      <h1 className="text-center py-3 text-5m">
+        Copyright © 2024 - All right by reserved by ACME Industries Ltd</h1>
+     </div>
+    </>
+  )
+}
+
+export default Footer
